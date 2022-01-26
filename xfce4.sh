96b4c6aabc48d31202096d458bae7e7b09a162eb
GOBMi-ux <89739451+GOBMi-ux@users.noreply.github.com>
2022-01-26 16:39:11 +0100

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -19,11 +19,17 @@ sudo apt install firefox -y > /dev/null 2>&1
 echo "===================================="
 echo "Install Brave"
 echo "===================================="
-sudo apt install brave -y > /dev/null 2>&1
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
 echo "===================================="
 echo "Install Winehq-stable"
 echo "===================================="
-sudo apt install winehq-stable -y > /dev/null 2>&1
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install wine64 wine32
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
